a311c1d5d0b14c6c5e715ff570182218764170e9
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-17 16:57:55 -0500

    Store DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { contadorReducer } from './contador/contador.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +18,11 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   imports: [
     BrowserModule,
     StoreModule.forRoot({contador: contadorReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
     AppRoutingModule
   ],
   providers: [],
