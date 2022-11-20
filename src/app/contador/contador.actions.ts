f5b94b8928d291987e9cb1e82e2e963faf11238c
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-20 15:37:54 -0500

    Accion reset de nieto

--- src/app/contador/contador.actions.ts
+++ src/app/contador/contador.actions.ts
@@ -2,6 +2,7 @@ import { createAction, props } from '@ngrx/store';
 
 export const incrementar = createAction('[Contador] Incrementar');
 export const decrementar = createAction('[Contador] Decrementar');
+export const reset = createAction('[Contador] Reset');
 export const multiplicar = createAction(
     '[Contador] Multiplicar',
     props<{numero: number}>()
--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import * as actions from '../contador.actions';
 
 @Component({
   selector: 'app-nieto',
@@ -8,17 +11,17 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class NietoComponent implements OnInit {
 
-  @Input() contador!: number;
-  @Output() cambioContador = new EventEmitter<number>;
+  contador!: number;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.select('contador')
+      .subscribe(contador => this.contador = contador);
   }
 
   reset() {
-    this.contador = 0;
-    this.cambioContador.emit(0);
+    this.store.dispatch(actions.reset());
   }
 
 }
